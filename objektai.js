3b56d54293f00c63f4ce958c7d80c83a0c3a69d3
EgidijusNek <egidijusnek@gmail.com>
2019-04-08 19:49:05 +0300

    pridėti duomenys

--- objektai.js
+++ objektai.js
@@ -13,6 +13,21 @@ var klase = [
             miestas: 'Ilgakiemio km.',
             salis: "Lenkija"
         }
+    },
+    {
+        vardas: "Egidijus",
+        banko_pin: 2545,
+        amzius: 33,
+        gyvunas: [
+            {suo: "Mazgė"}
+        ],
+        adresas:{
+            gatve: "J. Basanavičiaus al.",
+            rajonas: "Žaliakalnis",
+            namoNr: 62,
+            miestas: 'Kaunas',
+            salis: "Lietuva"
+        }
     }
 ];
 
