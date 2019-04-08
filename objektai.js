b18a6b31c1e2ade9797bfac70ba9c3ef8b9b90a8
egle <pushis77@gmail.com>
2019-04-08 19:52:23 +0300

    Egle

--- objektai.js
+++ objektai.js
@@ -1,19 +1,36 @@
 var klase = [
     {
         vardas: "Tautvydas",
-        banko_pin: 1234,
-        amzius: 33,
-        vaikai: [
-            {vardas: "Amelija"}
+    banko_pin: 1234,
+    amzius: 33,
+    vaikai: [
+    {vardas: "Amelija"}
+],
+    adresas:{
+    gatve: "Gatvinė g.",
+        rajonas: "Kauno raj.",
+        namoNr: 777,
+        miestas: 'Ilgakiemio km.',
+        salis: "Lenkija"
+}
+}
+],
+    vardas: "Egle",
+    banko_pin: 4444,
+    amzius: 41,
+    vaikai: [
+        {vardas: "Migle"},
+        {vardas:"Aleksandras"},
+        {vardas:"suo Kira"}
         ],
-        adresas:{
-            gatve: "Gatvinė g.",
-            rajonas: "Kauno raj.",
-            namoNr: 777,
-            miestas: 'Ilgakiemio km.',
-            salis: "Lenkija"
-        }
+    adresas:{
+        gatve: "Genio g.",
+        rajonas: "Kauno raj.",
+        namoNr: 31,
+        miestas: 'Vaisvydavos km.',
+        salis: "Lietuva"
     }
+}
 ];
 
 console.log("Klasiokų kiekis: ", klase.length);
