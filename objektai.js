b5552638125f4df48f2a070700999132410401d7
romanasV <romanasvenckus@gmail.com>
2019-04-08 19:42:56 +0300

    obj added

--- objektai.js
+++ objektai.js
@@ -1,19 +1,30 @@
 var klase = [
-    {
-        vardas: "Tautvydas",
-        banko_pin: 1234,
-        amzius: 33,
-        vaikai: [
-            {vardas: "Amelija"}
-        ],
-        adresas:{
-            gatve: "Gatvinė g.",
-            rajonas: "Kauno raj.",
-            namoNr: 777,
-            miestas: 'Ilgakiemio km.',
-            salis: "Lenkija"
-        }
+  {
+    vardas: "Tautvydas",
+    banko_pin: 1234,
+    amzius: 33,
+    vaikai: [{ vardas: "Amelija" }],
+    adresas: {
+      gatve: "Gatvinė g.",
+      rajonas: "Kauno raj.",
+      namoNr: 777,
+      miestas: "Ilgakiemio km.",
+      salis: "Lenkija"
     }
+  },
+  {
+    vardas: "Romanas",
+    banko_pin: 1234123412341234,
+    amzius: 30,
+    vaikai: [null],
+    adresas: {
+      gatve: "gatve",
+      rajonas: "Kauno",
+      namoNr: 15,
+      miestas: "gatve",
+      salis: "Lietuva"
+    }
+  }
 ];
 
 console.log("Klasiokų kiekis: ", klase.length);
