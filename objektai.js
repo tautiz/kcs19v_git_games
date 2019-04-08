d8c39d16540413b604ea257bd63210de8e3bd939
romanasV <romanasvenckus@gmail.com>
2019-04-08 20:17:47 +0300

    conf fix

--- objektai.js
+++ objektai.js
@@ -1,5 +1,4 @@
 var klase = [
-<<<<<<< HEAD
   {
     vardas: "Tautvydas",
     banko_pin: 1234,
@@ -51,66 +50,8 @@ var klase = [
       namoNr: 15,
       miestas: "gatve",
       salis: "Lietuva"
-=======
-    {
-        vardas: "Tautvydas",
-        banko_pin: 1234,
-        amzius: 33,
-        vaikai: [
-            {vardas: "Amelija"}
-        ],
-        adresas: {
-            gatve: "Gatvinė g.",
-            rajonas: "Kauno raj.",
-            namoNr: 777,
-            miestas: 'Ilgakiemio km.',
-            salis: "Lenkija"
-        }
-    },
-    {
-        vardas: "Jolita",
-        banko_pin: 1521,
-        amzius: 35,
-        vaikai: [
-            {vardas: "Ema"}
-        ],
-        adresas:{
-            gatve: "Alyvu g.",
-            rajonas: "Kauno raj.",
-            namoNr: 13,
-            miestas: 'Smiltynu km.',
-            salis: "Lietuva"
-        }
-    },
-    {
-        vardas: "Egidijus",
-        banko_pin: 2545,
-        amzius: 33,
-        gyvunas: [
-            {suo: "Mazgė"}
-        ],
-        adresas: {
-            gatve: "J. Basanavičiaus al.",
-            rajonas: "Žaliakalnis",
-            namoNr: 62,
-            miestas: 'Kaunas',
-            salis: "Lietuva"
-        }
-    },
-    {
-        vardas: "Romanas",
-        banko_pin: 1111,
-        amzius: 30,
-        vaikai: [null],
-        adresas: {
-            gatve: "gatve",
-            rajonas: "Kauno",
-            namoNr: 15,
-            miestas: "gatve",
-            salis: "Lietuva"
-        }
->>>>>>> 6a1de501ed99c49989add23b1fc0c7ce6bed64d5
     }
+  }
 ];
 
 console.log("Klasiokų kiekis: ", klase.length);
