6c767082133fc6037a9622b37f0ba1350ca17bd2
Tautvydas <tautvydas@dulskis.eu>
2019-04-08 18:28:42 +0200

    Pirmasis komitas

---
+++ objektai.js
@@ -0,0 +1,19 @@
+var klase = [
+    {
+        vardas: "Tautvydas",
+        banko_pin: 1234,
+        amzius: 33,
+        vaikai: [
+            {vardas: "Amelija"}
+        ],
+        adresas:{
+            gatve: "Gatvinė g.",
+            rajonas: "Kauno raj.",
+            namoNr: 777,
+            miestas: 'Ilgakiemio km.',
+            salis: "Lenkija"
+        }
+    }
+];
+
+console.log("Klasiokų kiekis: ", klase.length);
