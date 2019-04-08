6af3810be80e5ca7897a7e2c40f2b9280b7d354f
Imantas <pepermintas@yahoo.com>
2019-04-08 19:52:43 +0300

    Imantas

--- objektai.js
+++ objektai.js
@@ -13,7 +13,22 @@ var klase = [
             miestas: 'Ilgakiemio km.',
             salis: "Lenkija"
         }
-    }
+    },
+    {
+        vardas: "Imantas",
+        banko_pin: 5537,
+        amzius: 25,
+        vaikai: [
+            {vardas: "Pūkė"}
+        ],
+        adresas:{
+            gatve: "Šalkauskio g.",
+            rajonas: "Šiaulių raj.",
+            namoNr: 16,
+            miestas: 'Konfilktinis',
+            salis: "Wonderland"
+        }
+    },
 ];
 
 console.log("Klasiokų kiekis: ", klase.length);
