a1b6cb9f2a8b259a0074af03a7b0a017b7ac0097
Vita <vitvait@yahoo.com>
2019-04-08 21:01:47 +0300

    Vita2

--- objektai.js
+++ objektai.js
@@ -1,34 +1,49 @@
 var klase = [
-    {
-        vardas: "Tautvydas",
-        banko_pin: 1234,
-        amzius: 33,
-        vaikai: [
-            {vardas: "Amelija"}
-        ],
-        adresas: {
-            gatve: "Gatvinė g.",
-            rajonas: "Kauno raj.",
-            namoNr: 777,
-            miestas: 'Ilgakiemio km.',
-            salis: "Lenkija"
-        }
-    },
-    {
-        vardas: "Egidijus",
-        banko_pin: 2545,
-        amzius: 33,
-        gyvunas: [
-            {suo: "Mazgė"}
-        ],
-        adresas: {
-            gatve: "J. Basanavičiaus al.",
-            rajonas: "Žaliakalnis",
-            namoNr: 62,
-            miestas: 'Kaunas',
-            salis: "Lietuva"
-        }
-        ,
+        {
+            vardas: "Tautvydas",
+            banko_pin: 1234,
+            amzius: 33,
+            vaikai: [
+                {vardas: "Amelija"}
+            ],
+            adresas: {
+                gatve: "Gatvinė g.",
+                rajonas: "Kauno raj.",
+                namoNr: 777,
+                miestas: 'Ilgakiemio km.',
+                salis: "Lenkija"
+            }
+        },
+        {
+            vardas: "Egidijus",
+            banko_pin: 2545,
+            amzius: 33,
+            gyvunas: [
+                {suo: "Mazgė"}
+            ],
+            adresas: {
+                gatve: "J. Basanavičiaus al.",
+                rajonas: "Žaliakalnis",
+                namoNr: 62,
+                miestas: 'Kaunas',
+                salis: "Lietuva"
+            }
+        },
+        {
+            vardas: "Vita",
+            banko_pin: 4567,
+            amzius: 37,
+            gyvunas: [
+                {katinas: "katinas"}
+            ],
+            adresas: {
+                gatve: "Aguonu g.",
+                rajonas: "Eiguliai",
+                namoNr: 16,
+                miestas: 'Kauno m.',
+                salis: "Latvija"
+            }
+        },
         {
             vardas: "Romanas",
             banko_pin:
